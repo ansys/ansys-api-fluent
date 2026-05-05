266a030ee3e3970f02bbf542facc13ba38260437
Prithwish Mukherjee <prmukher@synopsys.com>
2026-05-05 14:38:42 +0530

    Remove PyAnsys logo.

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # Make the local Sphinx extensions importable
 sys.path.insert(0, os.path.abspath("_ext"))
 
-from ansys_sphinx_theme import ansys_favicon, pyansys_logo_black
+from ansys_sphinx_theme import ansys_favicon
 
 project = "ansys-api-fluent"
 copyright = "(c) 2026 ANSYS, Inc. All rights reserved"
@@ -46,13 +46,7 @@ exclude_patterns = [
 
 html_theme = "ansys_sphinx_theme"
 html_static_path = ["source/_static"]
-html_logo = pyansys_logo_black
 html_favicon = ansys_favicon
-
-# Custom stylesheet for the auto-generated proto reference fragments.
-html_css_files = [
-    "proto_docgen.css",
-]
 
 html_theme_options = {
     "github_url": "https://github.com/ansys/ansys-api-fluent",
@@ -65,7 +59,7 @@ html_theme_options = {
     "collapse_navigation": True,
 }
 
-# Copy button: strip Python prompts from copied code
+
 copybutton_prompt_text = r">>> ?|\.\.\. "
 copybutton_prompt_is_regexp = True
 
@@ -86,7 +80,6 @@ latex_elements = {
     "pointsize": "10pt",
 }
 
-# Suppress warnings
 suppress_warnings = [
     "myst.header_anchor",
 ]
